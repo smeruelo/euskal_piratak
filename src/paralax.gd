272d500f58a660ed9ebae3fe89fdc26cd7b59352
Sonia Meruelo <smeruelo@gmail.com>
2024-07-25 20:11:45 +0200

    Paralax

---
+++ src/paralax.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export var scroll_speed : float = -100
+@export var width : float = 400
+@onready var _big_cloud_1 : Sprite2D = $BigCloud1
+@onready var _big_cloud_2 : Sprite2D = $BigCloud2
+@onready var _small_cloud_1 : Sprite2D = $SmallCloud1
+@onready var _small_cloud_2 : Sprite2D = $SmallCloud2
+
+
+func _process(delta):
+	_move_cloud(_big_cloud_1, scroll_speed * delta)
+	_move_cloud(_big_cloud_2, scroll_speed * delta)
+	_move_cloud(_small_cloud_1, scroll_speed * delta / 5)
+	_move_cloud(_small_cloud_2, scroll_speed * delta / 3)
+	
+
+func _move_cloud(cloud, distance):
+	cloud.position.x += distance
+	if cloud.position.x < width * -1 :
+		cloud.position.x += width * 2
+		
+	
