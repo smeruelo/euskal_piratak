f4ca1772cfe65a51b78c6677ded56afae07332ac
Sonia Meruelo <smeruelo@gmail.com>
2024-07-25 18:16:35 +0200

    Camera offset

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,6 +1,11 @@
 extends Camera2D
 
 @export var _subject : Node2D
+@export var _offset : Vector2
+
+
+func _ready():
+	position.x = _subject.position.x + offset.x
 
 func _process(_delta):
-	position.x = _subject.position.x	
+	position.x = _subject.position.x + offset.x
