f24eeba206f866f1fbd5273591aad91900b24711
Daniel Sánchez <daniel.sanchez@digipen.edu>
2024-07-25 16:25:03 +0200

    Initial player code + animations

---
+++ player.gd
@@ -0,0 +1,58 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var is_attacking = false
+
+func _physics_process(delta):
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+		
+		if is_attacking:
+			$AnimatedSprite2D.play("attack")
+		elif is_on_floor():
+			$AnimatedSprite2D.play("walk")
+		else:
+			$AnimatedSprite2D.play("jump")
+			
+		if direction < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_attacking:
+			$AnimatedSprite2D.play("attack")
+		elif is_on_floor():
+			$AnimatedSprite2D.play("idle")
+		else:
+			$AnimatedSprite2D.play("jump")
+	
+	# Handle attack.
+	if Input.is_action_just_pressed("attack"):
+		is_attacking = true
+		$AnimatedSprite2D.play("attack")
+			
+	move_and_slide()
+
+func _on_animated_sprite_2d_animation_looped():
+
+	if $AnimatedSprite2D.animation == "attack":
+		is_attacking = false
