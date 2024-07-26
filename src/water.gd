de05698d0afbf2cdcf35ca2c4888bed3f4182554
Sonia Meruelo <smeruelo@gmail.com>
2024-07-26 04:58:21 +0200

    Water & water splash

---
+++ src/water.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var _splash : PackedScene
+
+func _spawn_splash(x : float):
+	var splash = _splash.instantiate()
+	add_child(splash)
+	splash.global_position.x = x
+	
+	
+func _on_body_entered(body : Node2D):
+	print(body)
+	_spawn_splash(body.position.x)
+	#body.die()
